baa3b0318f0598cd3ca6b4fa334e889daef4ffbb
danielmordi <danielmordi@gmail.com>
2023-03-13 00:35:26 +0100

    link updates new try name attribute added to form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -37,19 +37,19 @@ const Contact = () => {
             <form action="https://getform.io/f/ff64c9d1-b01b-4dbe-8d2a-a66ba59aaf60" method='POST'>
               <Row>
                 <Col sm={6} className='px-1'>
-                  <input type="text" value={formDetails.firstName} placeholder='First Name' onChange={(e) => onFormUpdate('firstName', e.target.value)} />
+                  <input type="text" name='firstName' value={formDetails.firstName} placeholder='First Name' onChange={(e) => onFormUpdate('firstName', e.target.value)} />
                 </Col>
                 <Col sm={6} className='px-1'>
-                  <input type="text" value={formDetails.lastName} placeholder='Last Name' onChange={(e) => onFormUpdate('lastName', e.target.value)} />
+                  <input type="text" name='lastName' value={formDetails.lastName} placeholder='Last Name' onChange={(e) => onFormUpdate('lastName', e.target.value)} />
                 </Col>
                 <Col sm={6} className='px-1'>
-                  <input type="email" value={formDetails.email} placeholder='Email Address' onChange={(e) => onFormUpdate('email', e.target.value)} />
+                  <input type="email" name='email' value={formDetails.email} placeholder='Email Address' onChange={(e) => onFormUpdate('email', e.target.value)} />
                 </Col>
                 <Col sm={6} className='px-1'>
-                  <input type="tel" value={formDetails.phone} placeholder='Phone No.' onChange={(e) => onFormUpdate('phone', e.target.value)} />
+                  <input type="tel" name='tel' value={formDetails.phone} placeholder='Phone No.' onChange={(e) => onFormUpdate('phone', e.target.value)} />
                 </Col>
                 <Col  className='px-1'>
-                  <textarea row='6' value={formDetails.message} placeholder='Message' onChange={(e) => onFormUpdate('message', e.target.value)} />
+                  <textarea row='6' name='message' value={formDetails.message} placeholder='Message' onChange={(e) => onFormUpdate('message', e.target.value)} />
                   <button type='submit'><span>Let's talk</span></button>
                 </Col>
               </Row>
